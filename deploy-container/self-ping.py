6ae64bd31f6079b0784e2ba599608acff5da75e9
cihuuy <95659029+cihuuy@users.noreply.github.com>
2022-08-19 09:50:46 +0530

    Create self-ping.py

---
+++ deploy-container/self-ping.py
@@ -0,0 +1,18 @@
+import os
+from time import sleep
+if __name__ == "__main__":
+    if os.getenv("NO_SLEEP") == "1":
+        if "APP_NAME" not in os.environ:
+            print("APP_NAME unset, terminating...")
+            exit()
+        app_name = os.getenv("APP_NAME")
+        while True:
+            try:
+                os.system(f"curl https://{app_name}.herokuapp.com/?folder=/home/coder/project")
+            except:
+                print("Ping failed, retrying...")
+                try:
+                    os.system(f"curl -s https://{app_name}.herokuapp.com/?folder=/home/coder/project")
+                except:
+                    print("Cannot ping app, terminating...")
+            sleep(600000)
